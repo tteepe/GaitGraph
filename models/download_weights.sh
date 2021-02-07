4a7a0117d1dc23cee879303c10ce08346c63e4c4
Torben Teepe <t.teepe@tum.de>
2021-02-07 15:21:55 +0100

    Fix yolov3 weight load #1

--- models/download_weights.sh
+++ models/download_weights.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Download weights for vanilla YOLOv3
-curl -O https://pjreddie.com/media/files/yolov3.weights
+curl -O https://pjreddie.com/media/files/yolov3.weights --header "Referer: pjreddie.com"
 # Download weights for tiny YOLOv3
-curl -O https://pjreddie.com/media/files/yolov3-tiny.weights
+curl -O https://pjreddie.com/media/files/yolov3-tiny.weights --header "Referer: pjreddie.com"
 ## Download weights for backbone network
-#curl -O https://pjreddie.com/media/files/darknet53.conv.74
+#curl -O https://pjreddie.com/media/files/darknet53.conv.74 --header "Referer: pjreddie.com"
 
 # Download pre-trained weights
 curl -LJO https://github.com/tteepe/GaitGraph/releases/download/v0.1/gaitgraph_resgcn-n39-r8_coco_seq_60.pth
