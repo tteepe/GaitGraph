93fa1487c430b17e8873c711f21dc79fdd4d2f27
Torben Teepe <t.teepe@tum.de>
2021-05-03 12:56:12 +0200

    Fix evaluation script name in experiments

--- experiments/3_1c_evaluate_train_sort_test_shuffle.sh
+++ experiments/3_1c_evaluate_train_sort_test_shuffle.sh
@@ -1,6 +1,6 @@
 cd ../src || exit
 
-python3 train.py casia-b \
+python3 evaluate.py casia-b \
                  ../save/casia-b_models/<INSERT NAME>/ckpt_epoch_best.pth \
                  ../data/casia-b_pose_test.csv \
                  --network_name resgcn-n39-r8 \
